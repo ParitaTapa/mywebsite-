3ef914656d46177a2995215a89f4cfbeffca5c45
ParitaTapa <97943410+ParitaTapa@users.noreply.github.com>
2022-02-22 14:03:09 +0700

    Create font.php

---
+++ font.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>กำหนดรูปแบบตัวอักษร</title>
+  <style>
+    @font-face{
+      font-family: myFont1;
+      src: url(smile Regular.ttf)
+     @font-face{
+      font-family: myFont2;
+      src: url(DM Weekend Regular.ttf)
+    }
+     @font-face{
+      font-family: myFont1;
+       @font-face{
+      font-family: myFont2;
+  </style>
+</head>
+<body>
+  <h1> id="font1">นางสาวปาริตา ทะปา ม.4/13 เลขที่20</h1>
+  <h2> id="font2">นางสาวปาริตา ทะปา ม.4/13 เลขที่20</h2>
+</body>
+</html>
+f0nt.com
